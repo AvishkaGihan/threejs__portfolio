4be7b5747e43c82c678ea2f5e168dd0849d56ce8
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-12-27 23:55:12 +0530

    add Contact, WorkExperience, and Footer sections; implement useAlert hook and Alert component; update favicon and index title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import About from "./sections/About";
+import Contact from "./sections/Contact";
+import WorkExperience from "./sections/Experience";
+import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
@@ -10,6 +13,9 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
+      <WorkExperience />
+      <Contact />
+      <Footer />
     </main>
   );
 };
